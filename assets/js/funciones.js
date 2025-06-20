3a94335024f23e1e7e3466e7948b2ffbafd15870
BioTRaX <ruizdiazalejandro87@gmail.com>
2025-06-20 17:39:20 -0300

    docs: agregar politicas de comentarios y cabeceras

--- assets/js/funciones.js
+++ assets/js/funciones.js
@@ -1,3 +1,8 @@
+/*
+# Nombre: funciones.js
+# Ubicación: assets/js/funciones.js
+# Descripción: Funciones utilitarias para mostrar mensajes en la tienda
+*/
 // Funciones básicas para la tienda
 function mostrarMensaje(mensaje) {
     alert(mensaje);
